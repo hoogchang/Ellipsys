14c6149f8808944938d4aa00de9fbf8f9a68133c
hoogchang <markchang93@gmail.com>
2016-06-01 12:59:53 -0700

    1.1 Added Butterworth @ 30Hz 2nd order Sample_Freq=4000

--- Impulse.h
+++ Impulse.h
@@ -1,6 +1,10 @@
 //---Impulse Disturbance Lead-Lag Compensator Biquad
     int         ImpulseController_ns = 1;     // number of sections
-    uint32_t    timeoutValue = 500;  // time interval - us; f_s = 2000 Hz
     static	struct	biquad ImpulseController[]={ 	 // define the array of floating point biquads
-        {7.928638e+00, -7.924383e+00, 0.000000e+00, 1.000000e+00, -9.921494e-01, 0.000000e+00, 0, 0, 0, 0, 0}
+        {7.943164e+00, -7.941032e+00, 0.000000e+00, 1.000000e+00, -9.960670e-01, 0.000000e+00, 0, 0, 0, 0, 0}
+        };
+    int         Butter_ns = 1;     // number of sections
+    uint32_t    timeoutValue = 250;  // time interval - us; f_s = 4000 Hz
+    static	struct	biquad Butter[]={ 	 // define the array of floating point biquads
+        {5.371698e-04, 1.074340e-03, 5.371698e-04, 1.000000e+00, -1.933380e+00, 9.355289e-01, 0, 0, 0, 0, 0}
         };
