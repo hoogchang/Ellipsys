d9a7567c7372760c5034681787372e1b99385eb9
hoogchang <markchang93@gmail.com>
2016-06-01 18:30:23 -0700

    1.2 Reduced cutoff Freq to 15Hz

--- Impulse.h
+++ Impulse.h
@@ -6,5 +6,5 @@
     int         Butter_ns = 1;     // number of sections
     uint32_t    timeoutValue = 250;  // time interval - us; f_s = 4000 Hz
     static	struct	biquad Butter[]={ 	 // define the array of floating point biquads
-        {5.371698e-04, 1.074340e-03, 5.371698e-04, 1.000000e+00, -1.933380e+00, 9.355289e-01, 0, 0, 0, 0, 0}
+        {1.365107e-04, 2.730214e-04, 1.365107e-04, 1.000000e+00, -1.966681e+00, 9.672274e-01, 0, 0, 0, 0, 0}
         };
